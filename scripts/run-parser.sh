abbf278b821cefb358b314b26d25f51103ff2010
pbv <dev@pb.vision>
2026-06-27 23:02:30 -0700

    add Apache-2.0 license

--- scripts/run-parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-
-cd "`dirname \"$0\"`"
-./compile-parser.sh
-node --experimental-default-type module ./run-parser.js
--- scripts/test-lexer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-
-cd "`dirname \"$0\"`"
-node --experimental-default-type module ./run-lexer.js
